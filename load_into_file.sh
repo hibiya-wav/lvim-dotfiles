5250e1f0beae31558c8c72df2d949205b2008dd7
hibiya-wav <danielsm535@gmail.com>
2025-04-16 22:15:13 -0400

    updated changes

--- load_into_file.sh
+++ load_into_file.sh
@@ -2,8 +2,8 @@
 
 # load from OS
 
-cat $HOME/.config/alacritty/alacritty.toml > alacritty.toml
+# cat $HOME/.config/alacritty/alacritty.toml > alacritty.toml
+# cat $HOME/.config/lvim/after/ftplugin/sql.lua > sql.lua
+# cat $HOME/.local/share/lunarvim/lvim/lua/lvim/core/theme.lua > theme.lua
+# cat $HOME/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua > lsp_config.lua
 cat $HOME/.config/lvim/config.lua > config.lua
-cat $HOME/.config/lvim/after/ftplugin/sql.lua > sql.lua
-cat $HOME/.local/share/lunarvim/lvim/lua/lvim/core/theme.lua > theme.lua
-cat $HOME/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua > lsp_config.lua
