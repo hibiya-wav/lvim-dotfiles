1fdd00ef30070edc0062025be72fa545d801dc33
Daniel Santos Martinez <danielsm535@gmail.com>
2025-08-05 15:33:40 -0400

    work new config for linux mint

--- config.lua
+++ config.lua
@@ -4,57 +4,70 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-
+-- General Vim options
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
--- lvim.colorscheme = "rose-pine-moon"
-lvim.colorscheme = "kanagawa-wave"
+lvim.colorscheme = "jb"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Custom key mappings
 lvim.keys.normal_mode["<leader>u"] = ":UndotreeToggle<return>"
 lvim.keys.normal_mode["<leader>z"] = ":ZenMode<return>"
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Terminal",
-  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
-  v = { "<cmd>2ToggleTerm size=75 direction=vertical<cr>", "Split vertical" },
-  h = { "<cmd>2ToggleTerm size=15 direction=horizontal<cr>", "Split horizontal" },
-}
-
 lvim.keys.normal_mode["<leader>rp"] = ":terminal<return>i"
 
-lvim.plugins = {
+lvim.builtin.which_key.mappings["t"] = {
+    name = "+Terminal",
+    f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+    v = { "<cmd>2ToggleTerm size=75 direction=vertical<cr>", "Split vertical" },
+    h = { "<cmd>2ToggleTerm size=15 direction=horizontal<cr>", "Split horizontal" },
+}
 
-  { "ellisonleao/gruvbox.nvim" },
-  {"mfussenegger/nvim-jdtls"},
-  {"rose-pine/neovim"},
-  {"vague2k/vague.nvim"},
-  {"rebelot/kanagawa.nvim"},
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = false
-        }
-      })
-    end,
-  },
-  {
-    "mbbill/undotree"
-  },
-  {
-    "folke/zen-mode.nvim",
-    config = function()
-      require("zen-mode").setup {}
-    end,
-  },
-  { "tpope/vim-dadbod" },
-  { "kristijanhusak/vim-dadbod-ui" },
-  -- , {"kristijanhusak/vim-dadbod-completion"}
-  {
-    "neovim/nvim-lspconfig",
-    ops = {
-      sqlls = {},
-    }
-  }
+-- Plugins
+lvim.plugins = {
+    { "ellisonleao/gruvbox.nvim" },
+    { "rose-pine/neovim" },
+    {
+        "nickkadutskyi/jb.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            -- require("jb").setup({transparent = true})
+            vim.cmd("colorscheme jb")
+        end,
+    },
+    { "vague2k/vague.nvim" },
+    { "rebelot/kanagawa.nvim" },
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    enable_close = true,
+                    enable_rename = true,
+                    enable_close_on_slash = false
+                }
+            })
+        end,
+    },
+    {
+        "mbbill/undotree"
+    },
+    {
+        "folke/zen-mode.nvim",
+        config = function()
+            require("zen-mode").setup {}
+        end,
+    },
+    { "tpope/vim-dadbod" },
+    { "kristijanhusak/vim-dadbod-ui" },
+    {
+        "neovim/nvim-lspconfig",
+        -- opts = {
+        --   sqlls = {},
+        -- }
+    },
 }
