215c6c436a0cdfa099ea11effe92e89bd6987d12
Hibiya <danielsm535@gmail.com>
2025-09-30 15:28:38 -0400

    rebase idk

--- scripts/LocalOS_To_RepositoryDotfiles.sh
+++ scripts/LocalOS_To_RepositoryDotfiles.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# load from OS
+# load from OS to files
 
-cat $HOME/.config/alacritty/alacritty.toml > alacritty.toml
+cd ../files/
 cat $HOME/.config/lvim/config.lua > config.lua
+cat $HOME/.config/alacritty/alacritty.toml > alacritty.toml
+cat $HOME/.tmux.conf > tmux.conf
+cat $HOME/.ideavimrc >ideavim
+cat $HOME/.bashrc > bashrc
+
 # cat $HOME/.config/lvim/after/ftplugin/sql.lua > sql.lua
 # cat $HOME/.local/share/lunarvim/lvim/lua/lvim/core/theme.lua > theme.lua
 # cat $HOME/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua > lsp_config.lua
--- scripts/RepositoryDotfiles_To_LocalOS.sh
+++ scripts/RepositoryDotfiles_To_LocalOS.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# dump to OS
+# dump files to OS
 
+cd ../files/
 cat config.lua > $HOME/.config/lvim/config.lua 
 cat alacritty.toml > $HOME/.config/alacritty/alacritty.toml
+cat tmux.conf > $HOME/.tmux.conf
+cat ideavim > $HOME/.ideavimrc
+cat bashrc > $HOME/.bashrc
+
 # cat sql.lua > $HOME/.config/lvim/after/ftplugin/sql.lua
 # cat theme.lua > $HOME/.local/share/lunarvim/lvim/lua/lvim/core/theme.lua
 # cat lsp_config.lua > $HOME/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua
